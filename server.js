f4639fde68cabf06a6557bef93d5e9f97379428e
Brandon <brandonroulstone71@gmail.com>
2024-05-24 10:30:10 +0200

    Initialized cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 // allows me to access my env variables
 config();
+import cors from 'cors';
 // initialize express server
 import express from 'express';
 import workshopRoute from "./Routes/workshopRoute.js"
@@ -9,6 +10,11 @@ const app = express();
 const PORT = process.env.MYSQL_ADDON_PORT;
 
 app.use(express.static('./Static'));
+app.use(cors({
+    origin: 'http://localhost:8080', // reminder update this to https://sharenet-theta.vercel.app/
+    credentials: true
+}))
+
 app.use(express.json());
 
 app.use('/workshops', workshopRoute)
